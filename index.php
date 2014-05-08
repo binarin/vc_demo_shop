04252b6aa45652a04b6e899103968ed5f3429ee0
Alexey Lebedeff <binarin@gmail.com>
2014-05-08 15:35:26 +0400

    Monolog

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php // -*- coding: utf-8-unix -*-
+
+require('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
+
+// add records to the log
+$log->addWarning("Running a beta version of Heroku's PHP support");
+
 echo "Hello, world!";
