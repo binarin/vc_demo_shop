e5127dea613438f537f8bcbb3c1654805a3f54aa
Alexey Lebedeff <binarin@gmail.com>
2014-05-08 22:50:56 +0400

    Page for customers returning from virtual cards system

--- order.php
+++ order.php
@@ -1,61 +1,72 @@
-<?php // -*- coding: utf-8-unix -*-
+<?php
+// -*- coding: utf-8-unix -*-
 class Order {
-    public $id;
-    public $amount;
-    public $status;
-    public $notifications;
+  public $id;
+  public $amount;
+  public $status;
+  public $notifications;
 
-    public function __construct($data) {
-        $this->setData($data);
-    }
+  public function __construct($data) {
+    $this->setData($data);
+  }
 
-    private function setData($data) {
-        $this->id = $data["id"];
-        $this->amount = $data["amount"];
-        $this->status = $data["status"];
-        $this->notifications = ["notifications" => [], "log" => []];
-        if ($data["notifications"]) {
-            $this->notifications = json_decode($data["notifications"], true);
+  private function setData($data) {
+    $this->id = $data["id"];
+    $this->amount = $data["amount"];
+    $this->status = $data["status"];
+    $this->notifications = ["notifications" => [], "log" => []];
+    if ($data["notifications"]) {
+      $this->notifications = json_decode($data["notifications"], true);
+      uasort(
+        $this->notifications["notifications"],
+        function ($a, $b) {
+          $a = $a["timestamp"]; $b = $b["timestamp"];
+          if ($a == $b) {
+            return 0;
+          }
+          return ($a < $b) ? -1 : 1;
         }
+      );
     }
+  }
 
-    public static function create($amount) {
-        $dbh = Config::dbh();
-        $sth = $dbh->prepare("insert into orders (amount) values(?) returning *");
-        $sth->execute(array($amount));
-        return new Order($sth->fetch(PDO::FETCH_ASSOC));
-    }
+  public static function create($amount) {
+    $dbh = Config::dbh();
+    $sth = $dbh->prepare("insert into orders (amount) values(?) returning *");
+    $sth->execute(array($amount));
+    return new Order($sth->fetch(PDO::FETCH_ASSOC));
+  }
 
-    public static function load($orderId) {
-        $dbh = Config::dbh();
-        $sth = $dbh->prepare("select * from orders where id = ?");
-        $sth->execute(array($orderId));
-        return new Order($sth->fetch(PDO::FETCH_ASSOC));
-    }
+  public static function load($orderId) {
+    $dbh = Config::dbh();
+    $sth = $dbh->prepare("select * from orders where id = ?");
+    $sth->execute(array($orderId));
+    return new Order($sth->fetch(PDO::FETCH_ASSOC));
+  }
 
-    public function addNotification($notification) {
-        $ts = gmstrftime("%Y-%m-%d %H:%M:%S");
-        $notificationId = $notification['notification_id'];
-        if (isset($this->notifications["notifications"][$notificationId])) {
-            $this->notifications["log"] []= "[$ts] Повторная нотификация {$notification['notification_id']}";
-        } else {
-            $this->notifications["notifications"][$notificationId] = $notification;
-            $this->notifications["log"] []= "[$ts] Получена нотификация {$notification['notification_id']}";
-        }
-        $this->save();
+  public function addNotification($notification) {
+    $ts = gmstrftime("%Y-%m-%d %H:%M:%S");
+    $notificationId = $notification['notification_id'];
+    if (isset($this->notifications["notifications"][$notificationId])) {
+      $this->notifications["log"] []= "[$ts] Повторная нотификация {$notification['notification_id']}";
+    } else {
+      $this->notifications["notifications"][$notificationId] = $notification;
+      $this->notifications["log"] []= "[$ts] Получена нотификация {$notification['notification_id']}";
     }
+    $this->save();
+  }
 
-    public function changeStatus($status) {
-        $ts = gmstrftime("%Y-%m-%d %H:%M:%S");
-        $this->notifications["log"] []= "[$ts] Смена статуса с '{$this->status}' на '$status'";
-        $this->status = $status;
-        $this->save();
-    }
+  public function changeStatus($status) {
+    $ts = gmstrftime("%Y-%m-%d %H:%M:%S");
+    $this->notifications["log"] []= "[$ts] Смена статуса с '{$this->status}' на '$status'";
+    $this->status = $status;
+    $this->save();
+  }
 
-    public function save() {
-        $dbh = Config::dbh();
-        $sth = $dbh->prepare("update orders set status = ?, notifications = ? where id = ? returning *");
-        $sth->execute([$this->status, json_encode($this->notifications), $this->id]);
-        $this->setData($sth->fetch(PDO::FETCH_ASSOC));
-    }
+  public function save() {
+    $dbh = Config::dbh();
+    $sth = $dbh->prepare("update orders set status = ?, notifications = ? where id = ? returning *");
+    $sth->execute([$this->status, json_encode($this->notifications), $this->id]);
+    $this->setData($sth->fetch(PDO::FETCH_ASSOC));
+  }
 }
--- pages.php
+++ pages.php
@@ -7,13 +7,70 @@ class Page {
       $page = new PayPage($matches["orderId"]);
     } elseif ($path_info === '/create') {
       $page = new CreatePage();
+    } elseif ($path_info === '/status') {
+      $page = new StatusPage();
     } elseif ($path_info === '/callback') {
       $page = new CallbackPage();
     } else {
       $page = new IndexPage();
     }
-
     return $page->run();
+  }
+}
+
+class StatusPage {
+  public function run() {
+    $config = new Config();
+    $api = new VirtualCards\API($config->vcUrl, $config->vcServiceId, $config->vcSecret);
+
+    if (
+      $api->validateSignature(
+        $_GET["signature"],
+        $_SERVER["REQUEST_METHOD"],
+        $config->publicUrl,
+        (isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : "/"),
+        $_SERVER["QUERY_STRING"]
+      )
+    ) {
+      $order = Order::load($_GET["order_id"]);
+      return ["title" => "Заказ {$order->id}",
+              "content" => $this->renderOrder($order)];
+    } else {
+      return ["title" => "Неправильная подпись",
+              "content" => "<h1>Нет прав на просмотр данной записи</h1>"];
+    }
+  }
+
+  private function renderOrder($order) {
+    ob_start(); ?>
+
+  <h1>Заказ <?php echo $order->id ?></h1>
+  <p>Статус: <?php echo $order->status ?></p>
+  <h2>Записи в логах</h2>
+  <ul>
+    <?php foreach ($order->notifications["log"] as $logRecord) : ?>
+      <li><?php echo $logRecord ?></li>
+    <?php endforeach; ?>
+  </ul>
+  <h2>Полученные нотификации</h2>
+  <table class="table">
+    <tr>
+      <th>Дата</td>
+      <th>Идентификатор</td>
+      <th>Статус</td>
+    </tr>
+    <?php foreach ($order->notifications["notifications"] as $id => $data) : ?>
+      <tr>
+        <td><?php echo $data["timestamp"] ?></td>
+        <td><?php echo $id ?></td>
+        <td><?php echo $data["status"] ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+    <?php
+    $result = ob_get_contents();
+    ob_end_clean();
+    return $result;
   }
 }
 
@@ -23,8 +80,6 @@ class CallbackPage {
 
     $config = new Config();
     $api = new VirtualCards\API($config->vcUrl, $config->vcServiceId, $config->vcSecret);
-
-    $_SERVER["HTTP_HOST"] = "vc-demo.herokuapp.com";
 
     if (
       $api->validateSignature(
@@ -40,7 +95,7 @@ class CallbackPage {
         $order->changeStatus($_GET["status"]);
       }
       $order->addNotification($_GET);
-      print_r($order);
+      echo "OK";
     } else {
       http_response_code(403);
     }
